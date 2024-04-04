86dd8d50711f13c074dd6f0644332383e5846cdc
Maria Vitoria <maria.suzarth@hotmail.com>
2024-04-04 00:56:47 -0300

    Atualização

--- atividade_consumo_api/CondicoesAtuais/core/urls.py
+++ atividade_consumo_api/CondicoesAtuais/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import index
 
 urlpatterns = [
-    path('', views.CondicoesAtuais),
+    path('', index, name='index'),
 ]
--- atividade_consumo_api/CondicoesAtuais/core/views.py
+++ atividade_consumo_api/CondicoesAtuais/core/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 import requests
 
-def CondicoesAtuais(request):  # Renomeado de 'requests' para 'request'
-    URL = "http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml"
-    resp = requests.get(URL)
+def index(request):
+    return render(request, 'index.html')
 
-    if resp.status_code == 200:
-        return HttpResponse(resp.content, content_type="application/xml")
-    else:
-        return HttpResponse("Erro ao acessar a API", status=resp.status_code)
+# def CondicoesAtuais(request):  # Renomeado de 'requests' para 'request'
+#     URL = "http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml"
+#     resp = requests.get(URL)
+
+    # if resp.status_code == 200:
+    #     return HttpResponse(resp.content, content_type="application/xml")
+    # else:
+    #     return HttpResponse("Erro ao acessar a API", status=resp.status_code)

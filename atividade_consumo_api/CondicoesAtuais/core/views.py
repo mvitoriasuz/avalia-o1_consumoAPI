dddfe870539b861794a12c9153ded9c7dc5fa6ee
Maria Vitoria <maria.suzarth@hotmail.com>
2024-04-04 20:22:21 -0300

    Tratativa de dados
    Caso não seja possível acessar a API, será apresentado o seguinte retorno Erro ao acessar a API

--- atividade_consumo_api/CondicoesAtuais/core/views.py
+++ atividade_consumo_api/CondicoesAtuais/core/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import HttpResponse
 import requests
 
 def CondicoesAtuais(request): 
-    URL = "http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml"
-    resp = requests.get(URL)
+    try:
+        URL = "http://servicos.cptec.inpe.br/XML/capitais/condicoesAtuais.xml"
+        resp = requests.get(URL)
 
-    if resp.status_code == 200:
-        return HttpResponse(resp.content, content_type="application/xml")
-    else:
-        return HttpResponse("Erro ao acessar a API", status=resp.status_code)
+        if resp.status_code == 200:
+            return HttpResponse(resp.content, content_type="application/xml")
+        else:
+            return HttpResponse("Erro ao acessar a API", status=resp.status_code)
+    except:
+        return HttpResponse("Erro ao conectar-se à API", status=500)
+
+
